a7287aac0a93291db6c00a02934e9854e7821fd5
Kevin Sasso <kevin.sasso@udc.edu>
2023-11-30 16:28:30 -0800

    kill-space

--- include/CRNN.h
+++ include/CRNN.h
@@ -16,16 +16,12 @@ struct TextResult
 	BoundingBox coords;
 };
 
-
-
 class CRNNModel : public TorchModel {
 
 public:
-
 	CRNNModel();
 	std::vector<TextResult> CRNNModel::recognize(std::vector<BoundingBox>& dets, cv::Mat& img, int& maxWidth);
 	torch::Tensor CRNNModel::preProcess(cv::Mat& det);
-	
 	torch::Tensor CRNNModel::normalizePad(cv::Mat& processed, int minWidth);
 	std::string CRNNModel::greedyDecode(torch::Tensor& input, int size);
 	//stores the last computed ratio (resize/rescale) from input image. 
@@ -33,6 +29,6 @@ public:
 	// need to support more languages.. english only for now
 	std::unordered_map<int,std::string> english;
 	// lol
-	std::vector<char> e{ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[','\\', ']', '^', '_', '`', '{', '|', '}', '~', ' ', '€', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+	std::vector<char> e{ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[','\\', ']', '^', '_', '`', '{', '|', '}', '~', ' ', 'â‚¬', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 };
 #endif
